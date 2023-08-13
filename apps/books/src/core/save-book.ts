6f41e6e13fae9cb8d78d60ce61f13cd7582702d1
Rui Costa <ruicosta042@gmail.com>
2023-08-13 22:52:05 +0100

    chore: reset branch

--- apps/books/src/core/save-book.ts
+++
@@ -1,8 +0,0 @@
-import type { Knex } from "knex";
-import { cuid } from "@ruicsh/helpers";
-import { cmsdb } from "@ruicsh/services";
-
-export async function saveBook(book: IBookToSave, db: Knex = cmsdb) {
-  const bookToSave = { ...book, id: book.id || cuid() };
-  await db("book").insert(bookToSave).onConflict("sourceUrl").merge();
-}
