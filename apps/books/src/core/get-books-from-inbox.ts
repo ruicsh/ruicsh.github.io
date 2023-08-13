f173ce32059c9538e76c5536bbf068b2c56a01b4
Rui Costa <ruicosta042@gmail.com>
2023-08-13 21:45:33 +0000

    feat(books): download inbox books from github

--- apps/books/src/core/get-books-from-inbox.ts
+++ apps/books/src/core/get-books-from-inbox.ts
@@ -1,18 +1,28 @@
-import path from "node:path";
+import { Readable } from "node:stream";
 
 import csv from "csvtojson";
 
 export async function getBooksFromInbox() {
-  const filename = path.join(process.cwd(), `inbox/books.csv`);
-  const data = await csv().fromFile(filename);
+  const url = new URL("https://raw.githubusercontent.com");
+  url.pathname = "/ruicsh/ruicsh.github.io/inbox/books.csv";
 
-  const books = data.map((book) => ({
-    ...book,
-    queuedOnDate: book.queuedOnDate ? book.queuedOnDate : undefined,
-    wishedOnDate: book.wishedOnDate ? book.wishedOnDate : undefined,
-    readOnDate: book.readOnDate ? book.readOnDate : undefined,
-    rating: book.rating ? Number(book.rating) : undefined,
-  }));
+  const response = await fetch(url.href);
+  if (!response?.body) {
+    throw new Error(`Can't find ${url.href}`);
+  }
+
+  // @ts-expect-error needs ReadableStream<any> instead of ReadableStream<Uint8Array>
+  const stream = Readable.fromWeb(response.body);
+
+  const books = await csv()
+    .fromStream(stream)
+    .subscribe((book) => ({
+      ...book,
+      queuedOnDate: book.queuedOnDate ? book.queuedOnDate : undefined,
+      wishedOnDate: book.wishedOnDate ? book.wishedOnDate : undefined,
+      readOnDate: book.readOnDate ? book.readOnDate : undefined,
+      rating: book.rating ? Number(book.rating) : undefined,
+    }));
 
   return books as IBookOnInbox[];
 }
