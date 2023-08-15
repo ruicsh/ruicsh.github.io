d922baf47d6ac518a9c94ba3b1c88544a186fe6d
Rui Costa <ruicosta042@gmail.com>
2023-08-15 23:55:05 +0000

    fix(render): use dynamic routes for books section

--- apps/render/src/app/books/[[...collection]]/page.tsx
+++ apps/render/src/app/books/[[...collection]]/page.tsx
@@ -6,22 +6,20 @@ export const metadata = {
 };
 
 interface IProps {
-  params: { collection: IBooksCollection };
+  params: { collection: IBooksCollection[] };
 }
 
 async function BooksCollectionPage(props: IProps) {
   const { params } = props;
-  const { collection } = params;
+  const [collection] = params.collection || [];
   const books = await getBooks({ collection });
 
-  return <Books books={books} />;
+  return <Books books={books} collection={collection} />;
 }
 
 export function generateStaticParams() {
-  return ["all", "wishlist", "queue", "read"].map((collection) => ({
-    params: {
-      collection,
-    },
+  return ["", "wishlist", "queue", "read"].map((collection) => ({
+    collection: [collection],
   }));
 }
 
