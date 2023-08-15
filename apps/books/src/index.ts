2b21f1fb295b2e963396e744d98e6f4c06ee019e
Rui Costa <ruicosta042@gmail.com>
2023-08-15 21:06:17 +0000

    fix(books): no need to export to CSV

--- apps/books/src/index.ts
+++ apps/books/src/index.ts
@@ -1,7 +1,6 @@
 import { cmsdbSchema } from "@ruicsh/cmsdb-schema";
 import { cmsdb, log } from "@ruicsh/services";
 
-import { exportToCsv } from "./core/export-to-csv";
 import { getBookDetails } from "./core/get-book-details";
 import { getBooksFromInbox } from "./core/get-books-from-inbox";
 import { saveBook } from "./core/save-book";
@@ -20,7 +19,6 @@ async function main() {
     await saveBook({ ...bookDetails, ...bookOnInbox });
   }
 
-  await exportToCsv();
   await cmsdb.destroy();
 
   log.info("Done.");
