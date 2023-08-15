fedea085ea6f05e9e8eeb4c1f8c236ec81c83b57
Rui Costa <ruicosta042@gmail.com>
2023-08-15 23:54:08 +0000

    fix(render): use knex to fetch data

--- apps/render/next.config.js
+++ apps/render/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
+  experimental: {
+    serverComponentsExternalPackages: ["knex"],
+  },
 };
 
 module.exports = nextConfig;
--- apps/render/src/data/books/index.ts
+++ apps/render/src/data/books/index.ts
@@ -1,6 +1,4 @@
-import path from "node:path";
-
-import csv from "csvtojson";
+import { cmsdb } from "@ruicsh/services";
 
 interface IGetBooksArgs {
   collection?: IBooksCollection;
@@ -9,31 +7,42 @@ interface IGetBooksArgs {
 export async function getBooks(args?: IGetBooksArgs) {
   const { collection } = args || ({} as IGetBooksArgs);
 
-  const booksCsvFile = path.join(process.cwd(), "shared/books.csv");
-  const data = (await csv().fromFile(booksCsvFile)) as IBook[];
+  const commonFields = [
+    "authors",
+    "cover",
+    "description",
+    "pageCount",
+    "publishedDate",
+    "publisher",
+    "subtitle",
+    "title",
+  ];
 
   let books: IBook[];
   if (collection === "read") {
-    books = data
-      .filter((book) => book.readOnDate)
-      .sort((a, b) => b.readOnDate!.localeCompare(a.readOnDate!));
+    books = await cmsdb("book")
+      .select([...commonFields, "readOnDate", "rating"])
+      .whereNot({ readOnDate: "" })
+      .orderBy("readOnDate", "desc");
   } else if (collection === "queue") {
-    books = data
-      .filter((book) => book.queuedOnDate)
-      .filter((book) => !book.readOnDate)
-      .sort((a, b) => b.queuedOnDate!.localeCompare(a.queuedOnDate!));
+    books = await cmsdb("book")
+      .select([...commonFields, "queuedOnDate"])
+      .whereNot({ queuedOnDate: "" })
+      .andWhere({ readOnDate: "" })
+      .orderBy("queuedOnDate", "desc");
   } else if (collection === "wishlist") {
-    books = data
-      .filter((book) => !book.queuedOnDate)
-      .filter((book) => !book.readOnDate)
-      .filter((book) => book.wishedOnDate)
-      .sort((a, b) => b.wishedOnDate!.localeCompare(a.wishedOnDate!));
+    books = await cmsdb("book")
+      .select([...commonFields, "wishedOnDate"])
+      .whereNot({ wishedOnDate: "" })
+      .andWhere({ queuedOnDate: "" })
+      .andWhere({ readOnDate: "" })
+      .orderBy("wishedOnDate", "desc");
   } else {
-    books = data.sort((a, b) => {
-      const aDate = a.readOnDate || a.queuedOnDate || a.wishedOnDate;
-      const bDate = b.readOnDate || b.queuedOnDate || b.wishedOnDate;
-      return bDate!.localeCompare(aDate!);
-    });
+    books = await cmsdb("book")
+      .select(commonFields)
+      .orderBy("wishedOnDate", "desc")
+      .orderBy("queuedOnDate", "desc")
+      .orderBy("readOnDate", "desc");
   }
 
   return books;
