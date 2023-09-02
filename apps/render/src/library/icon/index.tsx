8bef365c15e87b286ad9b0acca4ebfeb29a40c79
Rui Costa <ruicosta042@gmail.com>
2023-09-02 12:26:23 +0000

    fix(books): arrow/step icons

--- apps/render/src/library/icon/index.tsx
+++ apps/render/src/library/icon/index.tsx
@@ -3,11 +3,13 @@ import clsx from "clsx";
 import styles from "./index.module.scss";
 
 export enum IconType {
+  Arrow,
   Grid,
   List,
   StarEmpty,
   StarFilled,
   StarHalf,
+  Step,
 }
 
 interface IProps {
@@ -18,11 +20,13 @@ interface IProps {
 function Icon(props: IProps) {
   const { className, icon } = props;
   const cls = clsx(styles.root, className, {
+    [styles.iconArrow]: icon === IconType.Arrow,
     [styles.iconGrid]: icon === IconType.Grid,
     [styles.iconList]: icon === IconType.List,
     [styles.iconStarEmpty]: icon === IconType.StarEmpty,
     [styles.iconStarFilled]: icon === IconType.StarFilled,
     [styles.iconStarHalf]: icon === IconType.StarHalf,
+    [styles.iconStep]: icon === IconType.Step,
   });
 
   return <div aria-hidden="true" className={cls} role="img" />;
