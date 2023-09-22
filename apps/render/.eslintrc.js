195aa897199a2098ae2dac05939e0311fbbe1910
Rui Costa <ruicosta042@gmail.com>
2023-09-22 21:58:24 +0000

    build: remove eslint-config-next

--- apps/render/.eslintrc.js
+++ apps/render/.eslintrc.js
@@ -1,10 +1,6 @@
 const baseConfig = require("../../.eslintrc.json");
 
-baseConfig.extends = [
-  ...baseConfig.extends,
-  "next",
-  "plugin:jsx-a11y/recommended",
-];
+baseConfig.extends = [...baseConfig.extends, "plugin:jsx-a11y/recommended"];
 
 baseConfig.plugins = [...baseConfig.plugins, "jsx-a11y", "testing-library"];
 
