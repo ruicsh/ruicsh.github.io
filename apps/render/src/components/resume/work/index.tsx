8242a3ae4e6a5f43e986a9dc15c500e5d74f5809
Rui Costa <ruicosta042@gmail.com>
2024-03-24 12:37:04 +0000

    fix(resume): add better headline

--- apps/render/src/components/resume/work/index.tsx
+++ apps/render/src/components/resume/work/index.tsx
@@ -9,10 +9,17 @@ export function Work(props: IProps) {
 	const { work } = resume;
 
 	return (
-		<div className="col-start-6 col-end-12 flex flex-col gap-8">
-			{work.map((item, i) => (
-				<WorkItem key={`work-${i}`} item={item} order={i} />
-			))}
-		</div>
+		<>
+			<div className="col-start-6 col-end-12 flex flex-col gap-8">
+				{work.slice(0, 4).map((item, i) => (
+					<WorkItem key={`work-${i}`} item={item} />
+				))}
+			</div>
+			<div className="col-start-6 col-end-12 mt-8 flex flex-col gap-8 print:col-start-2 print:mt-8 print:break-before-all print:gap-4">
+				{work.slice(4, -1).map((item, i) => (
+					<WorkItem key={`work-${i}`} item={item} />
+				))}
+			</div>
+		</>
 	);
 }
--- apps/render/src/components/resume/work/item.tsx
+++ apps/render/src/components/resume/work/item.tsx
@@ -1,30 +1,23 @@
 type IProps = {
+	className?: string;
 	item: IJob;
-	order: number;
 };
 
 export function WorkItem(props: IProps) {
-	const { item, order } = props;
+	const { item } = props;
 	const { position, contract } = item;
 	const { company, market } = item;
 	const { startDate, endDate, duration } = item;
 	const { techStack } = item;
 	const { summary, highlights } = item;
 
-	const hasMarginTop = [4, 8].includes(order);
-
 	return (
-		<div
-			className="break-inside-avoid"
-			style={{
-				breakBefore: hasMarginTop ? "page" : "auto",
-			}}
-		>
+		<div className="break-inside-avoid">
 			<p className="text-xs uppercase">
 				{position}
 				{contract && <span> - contract</span>}
 			</p>
-			<h2 className="text-lg font-bold leading-normal">{company}</h2>
+			<h2 className="text-lg font-bold">{company}</h2>
 			<p className="text-sm font-semibold">{market}</p>
 			<p className="text-xs leading-normal">
 				{[startDate, endDate, duration].filter(Boolean).join(" - ")}
