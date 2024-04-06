ca3e16c418c17ef05904b6e96dd8db0cedeea458
Rui Costa <ruicosta042@gmail.com>
2024-04-06 23:55:37 +0000

    fix(render): don't use top-level await on static data

--- apps/render/src/static/index.ts
+++ apps/render/src/static/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prefer-top-level-await */
 import fs from "node:fs/promises";
 import path from "node:path";
 
@@ -5,12 +6,16 @@ import { cmsdb } from "@ruicsh/services";
 
 import { pressStaticBooks } from "./books";
 
-const staticDataDir = path.join(process.cwd(), "/public/static/data");
-const stat = await fs.stat(staticDataDir).catch(() => {});
-if (!stat) {
-	fs.mkdir(staticDataDir, { recursive: true });
+async function main() {
+	const staticDataDir = path.join(process.cwd(), "/public/static/data");
+	const stat = await fs.stat(staticDataDir).catch(() => {});
+	if (!stat) {
+		fs.mkdir(staticDataDir, { recursive: true });
+	}
+
+	await pressStaticBooks({ staticDataDir, cmsdb });
+
+	await cmsdb.destroy();
 }
 
-await pressStaticBooks({ staticDataDir, cmsdb });
-
-await cmsdb.destroy();
+main();
