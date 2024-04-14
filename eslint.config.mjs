53f2cefd751669ee9532c303bbf57deef1e4a889
Rui Costa <ruicosta042@gmail.com>
2024-04-14 12:39:03 +0000

    build: add eslint-plugin-react/react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import unicorn from "eslint-plugin-unicorn";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -11,7 +13,27 @@ export default [
 	unicorn.configs["flat/recommended"],
 	prettier,
 	{
+		files: ["**/*.tsx"],
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: { jsx: true },
+			},
+		},
+		settings: {
+			react: { version: "detect" },
+		},
+		plugins: {
+			"react-hooks": reactHooks,
+			react,
+		},
 		rules: {
+			...reactHooks.configs.recommended.rules,
+			...react.configs.recommended.rules,
+		},
+	},
+	{
+		rules: {
+			"react/react-in-jsx-scope": "off",
 			"unicorn/prevent-abbreviations": "off",
 			"unicorn/numeric-separators-style": [
 				"error",
